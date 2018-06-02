e61b6618b55bcb3bb559953dd75d229e4dd8ec4f
Bengt Martensson <barf@bengt-martensson.de>
2018-06-02 13:24:48 +0200

    Tweak examples.

--- examples/InfraredDecode/InfraredDecode.ino
+++ examples/InfraredDecode/InfraredDecode.ino
@@ -1,18 +1,26 @@
 // This sketch uses the IrReceiverSampler class of Infrared4Arduino (can be installed from Arduino
-// library manager). It reads IR signals with that class, and sends it 
+// library manager). It reads IR signals with that class, and sends it
 // to DecodeIR.
 
 // It requires a demodulating sensor connected to pin RECEIVE_PIN.
 
-// Sketch uses 111722 bytes of program storage space. 
+// Sketch uses 111722 bytes of program storage space.
 // Global variables use 2491 bytes (30%) of dynamic memory.
 // Will not run on anything considerably smaller than a Mega2560.
 
 #include <IrReceiverSampler.h>
 #include <decodePrintIr.h>
 
-#define RECEIVE_PIN 11//5U
+// Change to the pin where you have your receiver
+#define RECEIVE_PIN 5U
+
+// Get long signals in one chunk
+#define ENDTIMEOUT 100U
+
+// Max size of signals
 #define BUFFERSIZE 200U
+
+// Change if necessary
 #define BAUD 115200
 
 IrReceiver *receiver;
@@ -20,6 +28,7 @@ IrReceiver *receiver;
 void setup() {
     Serial.begin(BAUD);
     receiver = IrReceiverSampler::newIrReceiverSampler(BUFFERSIZE, RECEIVE_PIN);
+    receiver->setEndingTimeout(ENDTIMEOUT);
 }
 
 void loop() {
