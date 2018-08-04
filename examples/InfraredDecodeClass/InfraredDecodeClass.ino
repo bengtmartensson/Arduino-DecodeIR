a2e030d829300596cd26814df3d1518f5788ddaf
Bengt Martensson <barf@bengt-martensson.de>
2018-08-04 17:24:05 +0200

    Example sketch using LCD display. Resolves #1.

--- examples/InfraredDecodeClass/InfraredDecodeClass.ino
+++ examples/InfraredDecodeClass/InfraredDecodeClass.ino
@@ -1,37 +1,125 @@
-// This sketch uses the IrReceiverSampler class of Infrared4Arduino (can be installed from Arduino
-// library manager). It reads IR signals with that class, and sends it 
-// to DecodeIR.
+// This sketch uses the IrReceiverSampler class of Infrared4Arduino
+// (can be installed from Arduino library manager).
+// It reads IR signals with that class, and sends it
+// to DecodeIRClass, which is an OO-encapsulation of DecodeIR.
 
 // It requires a demodulating sensor connected to pin RECEIVE_PIN.
 
-// Sketch uses 111722 bytes of program storage space. 
+// Optionally, the decode is "printed" on an LCD display using the
+// LiquidCrystal_I2C library, version 1.1.2 or later.
+
+// Sketch uses 111722 bytes of program storage space.
 // Global variables use 2491 bytes (30%) of dynamic memory.
 // Will not run on anything considerably smaller than a Mega2560.
 
-#include <IrReceiverSampler.h>
-#include <DecodeIRClass.h>
+// Define to have output printed to the Serial.
+#define USE_SERIAL
+
+// Define to use an LCD display connected over I2C using the
+// LiquidCrystal_I2C library version 1.1.2 or later.
+#define LCD
+
+#define LCD_I2C_ADDRESS 0x3F
+#define LCD_COLUMNS 20
+#define LCD_ROWS 4
 
 #define RECEIVE_PIN 5U
-#define BUFFERSIZE 200U
+#define BUFFERSIZE 400U
 #define BAUD 115200
+#define ENDTIMEOUT 100U
+#define BEGINTIMEOUT 65535U
+
+#include <IrReceiverSampler.h>
+#include "DecodeIRClass.h"
+#ifdef LCD
+#include <LiquidCrystal_I2C.h>
+#endif
 
 IrReceiver *receiver;
+#ifdef LCD
+LiquidCrystal_I2C lcd(LCD_I2C_ADDRESS, LCD_COLUMNS, LCD_ROWS);
+#endif
 
 void setup() {
+#ifdef USE_SERIAL
     Serial.begin(BAUD);
+    while (!Serial)
+        ;
+    Serial.println(F("Welcome to DecodeIR!"));
+    Serial.println(F("Waiting for signal."));
+#endif
+
+#ifdef LCD
+    lcd.init();
+    lcd.backlight();
+    lcd.print(F("Welcome to DecodeIR!"));
+    lcd.setCursor(0, 1);
+    lcd.print(F("Waiting for signal."));
+#endif
+
     receiver = IrReceiverSampler::newIrReceiverSampler(BUFFERSIZE, RECEIVE_PIN);
+    receiver->setEndingTimeout(ENDTIMEOUT);
+    receiver->setBeginningTimeout(BEGINTIMEOUT);
 }
 
 void loop() {
     receiver->receive(); // combines enable, loop, disable
 
-    if (receiver->isEmpty())
-        Serial.println(F("timeout"));
-    else {
-        IrSequence *irSequence = receiver->toIrSequence();
-        DecodeIRClass decoder(*irSequence, IrSignal::defaultFrequency);
-        delete irSequence;
-        if (decoder.getProtocol()[0] != '\0')
-            Serial.println(decoder.getString1());
+    if (receiver->isEmpty()) {
+        //Serial.println(F("timeout"));
+        return;
     }
+
+    IrSequence *irSequence = receiver->toIrSequence();
+    DecodeIRClass decoder(*irSequence);
+    delete irSequence;
+    if (decoder.getProtocol()[0] == '\0') {
+#ifdef USE_SERIAL
+        Serial.println(F("** Undecodable **"));
+#endif
+        return;
+    }
+
+#ifdef USE_SERIAL
+    Serial.print(decoder.getProtocol());
+    Serial.print(" ");
+    Serial.print(decoder.getD());
+    Serial.print(" ");
+    if (decoder.getS() != -1) {
+        Serial.print(decoder.getS());
+        Serial.print(" ");
+    }
+    Serial.print(decoder.getF());
+    if (decoder.getMiscMessage()[0]) {
+        Serial.print(" ");
+        Serial.print(decoder.getMiscMessage());
+    }
+    if (decoder.getErrorMessage()[0]) {
+        Serial.print(" ");
+        Serial.print(decoder.getErrorMessage());
+    }
+    Serial.println();
+#endif
+
+#ifdef LCD
+    lcd.clear();
+    lcd.home();
+    lcd.print(decoder.getProtocol());
+    lcd.print(" ");
+    lcd.print(decoder.getD());
+    lcd.print(" ");
+    if (decoder.getS() != -1) {
+        lcd.print(decoder.getS());
+        lcd.print(" ");
+    }
+    lcd.print(decoder.getF());
+    lcd.setCursor(0, 1);
+    if (decoder.getMiscMessage()[0]) {
+        lcd.print(decoder.getMiscMessage());
+        lcd.print(" ");
+    }
+    if (decoder.getErrorMessage()[0]) {
+        lcd.print(decoder.getErrorMessage());
+    }
+#endif
 }
